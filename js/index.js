1c0aea4e0fd93ea2265d5a107971d8a6b3e13c92
CodeNinja1989 <153234904+CodeNinja1989@users.noreply.github.com>
2023-12-09 10:30:34 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-﻿// Get a reference to the button, select element, and the message div
+// Get a reference to the button, select element, and the message div
 const orderButton = document.getElementById("orderButton");
 const itemSelect = document.getElementById("itemSelect");
 const messageDiv = document.getElementById("message");
@@ -12,6 +12,16 @@ orderButton.addEventListener("click", function () {
     messageDiv.textContent = `Thank you for placing your order for a ${selectedItem}!`;
 });
 
+// JavaScript to toggle the visibility of the "About" information
+document.getElementById("aboutLink").addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent the link from navigating
 
+    var aboutInfo = document.getElementById("aboutInfo");
+    if (aboutInfo.style.display === "none" || aboutInfo.style.display === "") {
+        aboutInfo.style.display = "block";
+    } else {
+        aboutInfo.style.display = "none";
+    }
+});
 
 
